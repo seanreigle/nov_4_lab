f4f5813a314c9dadf09d3a39d794c392ad0f72cd
Sean Reigle <sereigle@gmail.com>
2017-03-24 14:33:09 -0700

    Changing stuff

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ function cm(inches){ conv1 = inches / (2.54);
 function inches(cm){ conv1 = cm * (2.54);
   return conv1;
 }
+
+var feet;
+
+function km(kilometers) { conv2 = km * (2.54);
+  return conv2;
+}
