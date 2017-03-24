367116a2fe7d70f88c63f66adf0c1f38111b71da
Fortunato <fortunato.maycotte@gmail.com>
2017-03-24 14:48:08 -0700

    more garbage

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ function cm(inches){ conv1 = inches / (2.54);
 function inches(cm){ conv1 = cm * (2.54);
   return conv1;
 }
+afafrghfsegsgthgrh
+hsg
+gf
+// HACK: ghgfhgf
+hgfh
+fgh
+fg
+gh
+fg
+gf
+gfhfgg
